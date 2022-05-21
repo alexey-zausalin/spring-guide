4b980fa17d5fdf7d5bcc857b3c90776d4e312f34
Alexey Zausalin <alexeyzausalin@gmail.com>
2022-05-21 22:42:44 +0300

    Init 'Consuming a RESTful Web Service' guide

---
+++ gs-consuming-rest/src/main/java/com/github/alexeyzausalin/springguides/gsconsumingrest/GsConsumingRestApplication.java
@@ -0,0 +1,13 @@
+package com.github.alexeyzausalin.springguides.gsconsumingrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsConsumingRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsConsumingRestApplication.class, args);
+	}
+
+}

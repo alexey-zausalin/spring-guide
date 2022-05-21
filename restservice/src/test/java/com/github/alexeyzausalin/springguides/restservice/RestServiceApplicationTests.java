2baa12e269b27fe0e9aefe6727dc69adaa87ddd0
Alexey Zausalin <alexeyzausalin@gmail.com>
2022-05-21 22:07:09 +0300

    Init 'Rest service' guide

---
+++ restservice/src/test/java/com/github/alexeyzausalin/springguides/restservice/RestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.alexeyzausalin.springguides.restservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

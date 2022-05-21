ae759828118bc22a5252006b565d40a4f4f300d5
Alexey Zausalin <alexeyzausalin@gmail.com>
2022-05-22 00:02:21 +0300

    Init 'gs-uploading-files.zip' guide

---
+++ gs-uploading-files/src/test/java/com/github/alexeyzausalin/springguides/gsuploadingfiles/GsUploadingFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.alexeyzausalin.springguides.gsuploadingfiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsUploadingFilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

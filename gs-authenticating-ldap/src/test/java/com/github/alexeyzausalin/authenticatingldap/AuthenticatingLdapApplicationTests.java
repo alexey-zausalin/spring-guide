fb20680483128b4140bacf2c58ae438f17bea541
Alexey Zausalin <alexeyzausalin@gmail.com>
2022-05-25 22:40:16 +0300

    Init "Authenticating a User with LDAP" project

---
+++ gs-authenticating-ldap/src/test/java/com/github/alexeyzausalin/authenticatingldap/AuthenticatingLdapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.alexeyzausalin.authenticatingldap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticatingLdapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
